506746b4681a99ebcab5f0ac6e5530d566c15f90
anhtai862 <leovince862@gmail.com>
2022-03-13 02:24:08 +0700

    test

---
+++ Pooch Care/main.js
@@ -0,0 +1,19 @@
+const checkbox = document.getElementById("checkbox");
+checkbox.addEventListener("change",() => {
+    // change theme of the website
+    document.body.classList.toggle("dark");
+    document.section.classList.toggle("dark");
+
+});
+
+var playButton = document.querySelector(".play__button");
+var videoContent = document.querySelector(".video__content");
+var closeVideo = document.querySelector ("#close__video");
+
+playButton.addEventListener("click",function () {
+    videoContent.style.display = "flex"
+})
+closeVideo.addEventListener("click",function () {
+    videoContent.style.display = "none"
+})
+
